9f90fc165832a77e04285d2649589b94f8e700db
laughter1998 <laughter98@hanmail.net>
2025-03-08 14:15:21 +0900

    섹션3 엔티티 테스트

--- src/test/java/org/zerock/spiserver2/repository/TodoRepositoryTests.java
+++ src/test/java/org/zerock/spiserver2/repository/TodoRepositoryTests.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.spiserver2.domain.Todo;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @SpringBootTest
 @Log4j2 // Lombok의 Log4j2 어노테이션
@@ -17,5 +21,44 @@ public class TodoRepositoryTests {
     public void test1(){
         Assertions.assertNotNull(todoRepository);
         log.info(todoRepository.getClass().getName());
+    }
+
+    @Test
+    public void testInsert(){
+        Todo todo = Todo.builder()
+                .title("Title")
+                .content("Content...")
+                .dueDate(LocalDate.of(2023,12,30))
+                .build();
+
+        Todo result = todoRepository.save(todo);
+
+        log.info(result);
+    }
+
+    @Test
+    public void testRead(){
+
+
+        Long tno = 1L;
+
+        Optional<Todo> result = todoRepository.findById(tno);
+        Todo todo = result.orElseThrow();
+
+        log.info(todo);
+    }
+
+    @Test
+    public  void testUpdate(){
+        //먼저 로딩 하고  엔티티 객체를 변경
+        Long tno = 1L;
+
+        Optional<Todo> result = todoRepository.findById(tno);
+        Todo todo = result.orElseThrow();
+
+        todo.changeTitle("Update Title");
+        todo.changeContent("Update Content");
+        todo.changeComplete(true);
+        todoRepository.save(todo);
     }
 }
