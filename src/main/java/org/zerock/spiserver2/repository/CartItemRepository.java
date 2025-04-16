f292404d86a86b9c5b6be910c7785794a882a2cd
laughter1998 <laughter98@hanmail.net>
2025-04-16 15:58:27 +0900

    섹션10 Repository개발 및 테스트 1

--- src/main/java/org/zerock/spiserver2/repository/CartItemRepository.java
+++ src/main/java/org/zerock/spiserver2/repository/CartItemRepository.java
@@ -23,4 +23,22 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     List<Object[]> getItemsOfCartDTOByEmail(@Param("email") String email);
 
+    @Query("select ci from CartItem ci left join Cart c on ci.cart = c " +
+            "where c.owner.email = :email and  ci.product.pno = :pno")
+    CartItem getItemOfPno(@Param("email") String email, @Param("pno") Long pno);
+
+    @Query("select c.cno from Cart c left join  CartItem ci on ci.cart = c where ci.cino = :cino")
+    Long getCartFromItem( @Param("cino") Long cino );
+
+    @Query("select " +
+            " new org.zerock.spiserver2.dto.CartItemListDTO(ci.cino, ci.qty, p.pname, p.price, pi.fileName ) " +
+            "from " +
+            "CartItem ci inner join Cart mc on ci.cart = mc" +
+            " left join Product p on ci.product = p" +
+            " left join p.imageList pi" +
+            " where " +
+            " pi.ord = 0 and mc.cno = :cno " +
+            " order by ci.cino desc ")
+    List<CartItemListDTO> getItemsOfCartDTOByCart(@Param("cno") Long cno );
+
 }
--- src/test/java/org/zerock/spiserver2/repository/CartRepositoryTests.java
+++ src/test/java/org/zerock/spiserver2/repository/CartRepositoryTests.java
@@ -4,6 +4,11 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.spiserver2.domain.CartItem;
+
+import java.util.Optional;
 
 @SpringBootTest
 @Log4j2
@@ -14,6 +19,23 @@ public class CartRepositoryTests {
 
     @Autowired
     private  CartItemRepository cartItemRepository;
+
+    @Transactional
+    @Commit
+    @Test
+    public void testInsertByProduct() {
+
+        String email = "suer1@aaa.com";
+        Long pno = 5L;
+        int qty = 1;
+
+        CartItem cartItem = cartItemRepository.getItemOfPno(email, pno);
+        if(cartItem != null) {
+            cartItem.changeQty(qty);
+            cartItemRepository.save(cartItem);
+            return;
+        }
+    }
 
     @Test
     public void testListOfMember() {
